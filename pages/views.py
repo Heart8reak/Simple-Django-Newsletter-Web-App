6ecfabaa16c9c430add5a7f83fa6344cb668c35e
Carlos Baez <carlosbaez@green-planet.attlocal.net>
2019-02-16 21:05:35 -0500

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def home_view(request):
+    return render(request, "home.html")
+def about_view(request):
+    return render(request, "about.html")
+def signup_view(request):
+    return render(request, "signup.html")
